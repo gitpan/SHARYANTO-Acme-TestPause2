5962131971e284c7ba004f9a32e1c8d824f37055
Steven Haryanto <stevenharyanto@gmail.com>
2012-11-14 03:00:42 +0000

    Import of SHARYANTO/SHARYANTO-Acme-TestPause2-0.01 from CPAN.
    gitpan-cpan-distribution: SHARYANTO-Acme-TestPause2
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/SHARYANTO-Acme-TestPause2-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/SHARYANTO/Acme/TestPause2.pm
@@ -0,0 +1,49 @@
+package SHARYANTO::Acme::TestPause2;
+
+use 5.010;
+use strict;
+use warnings;
+
+our $VERSION = '0.01'; # VERSION
+
+sub s1 {
+    say "s1 from SHARYANTO-Acme-TestPause2";
+}
+
+{
+    no warnings;
+    sub SHARYANTO::Acme::TestPause1::s1 {
+        say "s1 from SHARYANTO-Acme-TestPause2, clobbering SHARYANTO::Acme::TestPause1";
+    }
+}
+
+1;
+# ABSTRACT: Abstract
+
+
+__END__
+=pod
+
+=head1 NAME
+
+SHARYANTO::Acme::TestPause2 - Abstract
+
+=head1 VERSION
+
+version 0.01
+
+=for Pod::Coverage .*
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
